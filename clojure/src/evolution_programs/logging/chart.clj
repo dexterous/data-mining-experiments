67232ae063075c44e40693c55d8bf0856220056c
Saager Mhatre <saager.mhatre@gmail.com>
2015-04-11 18:21:57 -0400

    Update chart logger; detailed generation analysis
    - use box and whisker chart to provide statistics bout each generation

--- clojure/src/evolution_programs/logging/chart.clj
+++ clojure/src/evolution_programs/logging/chart.clj
@@ -2,16 +2,16 @@
   (:require [evolution-programs.util.population-statistic :as stat])
   (:import [javax.swing BoxLayout JFrame]
            [org.jfree.chart ChartFactory ChartPanel StandardChartTheme]
-           [org.jfree.data.category CategoryDataset DefaultCategoryDataset]
            [org.jfree.data.function Function2D]
            [org.jfree.data.general DatasetUtilities]
-           [org.jfree.data.xy XYDataset XYSeries XYSeriesCollection]))
+           [org.jfree.data.xy XYSeries XYSeriesCollection]
+           [org.jfree.data.statistics BoxAndWhiskerCategoryDataset DefaultBoxAndWhiskerCategoryDataset]))
 
 (ChartFactory/setChartTheme (StandardChartTheme. "JFree/Shadow" true))
 
 (defmulti ^:private chart class)
 
-(defmethod chart XYDataset [dataset]
+(defmethod chart XYSeriesCollection [dataset]
   (doto (ChartFactory/createXYLineChart "Curve of function" "X" "Y" dataset)
     (->
       (.getXYPlot)
@@ -20,12 +20,8 @@
         (.setSeriesLinesVisible 0 false)
         (.setSeriesShapesVisible 0 true)))))
 
-(defmethod chart CategoryDataset [dataset]
-  (doto (ChartFactory/createLineChart "Population Analysis" "Generation" "Fitness" dataset)
-    (->
-      (.getCategoryPlot)
-      (.getRenderer)
-      (.setShapesVisible true))))
+(defmethod chart BoxAndWhiskerCategoryDataset [dataset]
+  (ChartFactory/createBoxAndWhiskerChart "Population Analysis" "Generation" "Fitness" dataset false))
 
 (defn- panel [chart]
   (doto (ChartPanel. chart)
@@ -52,26 +48,30 @@
   (doto (XYSeriesCollection. (XYSeries. "Individual"))
     (.addSeries (DatasetUtilities/sampleFunction2DToSeries (->Function2D f) min max samples "Value"))))
 
+(defn- set-individuals [series f population]
+  (doseq [[individual] population]
+    (.add series individual (f individual) false)))
+
 (defn- log-generation
+  ([f generation-series analysis-dataset population generation-number]
+   (doto generation-series
+     (.clear)
+     (set-individuals f population)
+     (.fireSeriesChanged))
+   (log-generation analysis-dataset population generation-number))
   ([analysis-dataset population generation-number]
    (doto analysis-dataset
-     (.addValue (stat/mean-fitness population) "Mean" generation-number)
-     (.addValue (stat/best-fitness population) "Best" generation-number))
-   (Thread/sleep 500))
-  ([f generation-series analysis-dataset population generation-number]
-   (.clear generation-series)
-   (doseq [[individual] population]
-     (.add generation-series individual (f individual)))
-   (log-generation analysis-dataset population generation-number)))
+     (.add (stat/fitness population) "Fitness" generation-number))
+   (Thread/sleep 500)))
 
 (defn logger
   ([]
-   (let [analysis-dataset (DefaultCategoryDataset.)]
+   (let [analysis-dataset (DefaultBoxAndWhiskerCategoryDataset.)]
      (frame "Popluation Analysis" analysis-dataset)
      (partial log-generation analysis-dataset)))
   ([f min max samples]
    (let [sample-dataset (sample f min max samples)
          generation-series (.getSeries sample-dataset "Individual")
-         analysis-dataset (DefaultCategoryDataset.)]
+         analysis-dataset (DefaultBoxAndWhiskerCategoryDataset.)]
      (frame  "Population Analysis" sample-dataset analysis-dataset)
      (partial log-generation f generation-series analysis-dataset))))
