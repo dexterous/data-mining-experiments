916682d38c3f914d8f2cd0d859c169886fb9b099
Saager Mhatre <saager.mhatre@gmail.com>
2015-05-04 00:04:16 -0400

    add svm/linear refs

--- project.clj
+++ project.clj
@@ -5,6 +5,10 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-genetic "0.3.0" :exclusions [org.clojure/clojure]]
+                 [svm-clj "0.1.3"]
+               #_[hjrnunes/clj-liblinear "0.1.4"]
+               #_[de.bwaldvogel/liblinear "1.95"]
+               #_[edu.berkeley.compbio/jlibsvm "0.911"] ;;http://dev.davidsoergel.com/nexus/content/repositories/{releases,snapshots}
                  [org.jfree/jfreechart "1.0.19"]]
   :main ^:skip-aot clojure.main
   :source-paths ["src/main/clojure"]
