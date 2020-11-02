618574ade65f73d65ad2b499092f24a5e8582f50
Saager Mhatre <saager.mhatre@gmail.com>
2020-11-02 06:43:01 -0500

    Relayout charts using Boxes for simplicity

--- src/main/clojure/dm/viz/chart.clj
+++ src/main/clojure/dm/viz/chart.clj
@@ -1,6 +1,6 @@
 (ns dm.viz.chart
   (:import
-    (javax.swing BoxLayout JFrame SwingUtilities)
+    (javax.swing Box BoxLayout JFrame SwingUtilities)
     (org.jfree.chart ChartFactory ChartPanel StandardChartTheme)))
 
 (ChartFactory/setChartTheme (StandardChartTheme. "JFree/Shadow" true))
@@ -8,20 +8,21 @@
 (defmulti chart class)
 
 (defn- panel [chart]
-  (doto (ChartPanel. chart)
-    (.setMaximumDrawWidth 1600)
-    (.setMouseWheelEnabled true)))
+  (doto (Box. BoxLayout/Y_AXIS)
+    (.add (doto (ChartPanel. chart)
+            (.setMaximumDrawWidth 1600)
+            (.setMouseWheelEnabled true)))))
 
 (defn- add-charts [frame datasets]
-  (let [content-pane (.getContentPane frame)]
-    (.setLayout content-pane (BoxLayout. content-pane BoxLayout/Y_AXIS))
+  (let [box (Box. BoxLayout/Y_AXIS)]
+    (.setContentPane frame box)
     (doseq [dataset datasets]
-      (.add content-pane (panel (chart dataset))))))
+      (.add box (panel (chart dataset))))))
 
 (defn frame [title & datasets]
   (letfn [(show-later [f] (SwingUtilities/invokeLater #(.setVisible f true)))]
     (doto (JFrame. title)
-      (add-charts datasets)
       (.setExtendedState JFrame/MAXIMIZED_BOTH)
       (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE)
+      (add-charts datasets)
       (show-later))))
