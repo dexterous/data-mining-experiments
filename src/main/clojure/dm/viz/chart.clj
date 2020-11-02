0eeb50fa87285b43469c0c38dc92c81cb5ea2bc9
Saager Mhatre <saager.mhatre@gmail.com>
2020-11-02 06:49:49 -0500

    Add ChartEditor support

--- src/main/clojure/dm/viz/chart.clj
+++ src/main/clojure/dm/viz/chart.clj
@@ -1,17 +1,35 @@
 (ns dm.viz.chart
   (:import
-    (javax.swing Box BoxLayout JFrame SwingUtilities)
-    (org.jfree.chart ChartFactory ChartPanel StandardChartTheme)))
+    (javax.swing AbstractAction Box BoxLayout JButton JFrame JOptionPane SwingUtilities)
+    (org.jfree.chart ChartFactory ChartPanel StandardChartTheme)
+    (org.jfree.chart.editor ChartEditorManager)))
 
 (ChartFactory/setChartTheme (StandardChartTheme. "JFree/Shadow" true))
 
 (defmulti chart class)
 
+(defn- action [action-name handler-fn]
+  (proxy [AbstractAction] [action-name]
+    (actionPerformed [event] (handler-fn event))))
+
+(defn- edit-confirmed? [event editor]
+  (let [button (cast JButton (.getSource event))]
+    (= JOptionPane/OK_OPTION
+       (JOptionPane/showConfirmDialog button
+                                      editor
+                                      (.getText button)
+                                      JOptionPane/OK_CANCEL_OPTION
+                                      JOptionPane/PLAIN_MESSAGE))))
+
 (defn- panel [chart]
   (doto (Box. BoxLayout/Y_AXIS)
     (.add (doto (ChartPanel. chart)
             (.setMaximumDrawWidth 1600)
-            (.setMouseWheelEnabled true)))))
+            (.setMouseWheelEnabled true)))
+    (.add (JButton. (action "Customize Chart" (fn [e]
+                                                  (let [editor (ChartEditorManager/getChartEditor chart)]
+                                                    (if (edit-confirmed? e editor)
+                                                      (.updateChart editor chart)))))))))
 
 (defn- add-charts [frame datasets]
   (let [box (Box. BoxLayout/Y_AXIS)]
